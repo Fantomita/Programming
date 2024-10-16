ca9b344f0467e954a91e548b02b0279525239700
Fantomita <56206078+Fantomita@users.noreply.github.com>
2024-10-17 00:27:18 +0300

    Update Sub 25.sql

--- Atestat SQL/Sub 25.sql
+++ Atestat SQL/Sub 25.sql
@@ -6,4 +6,4 @@ if exists(select * from olimp where fel='B' and marime=42)
 else
 	select 'Nu exista articole barbatesti de marime 42' as mesaj
 --c
-select * from olimp where pret=(select max(pret) from olimp where fel='F')
+select * from olimp where pret=(select max(pret) from olimp where fel='F') and fel='F'
