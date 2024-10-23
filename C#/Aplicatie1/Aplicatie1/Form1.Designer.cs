b94963fa0fe7e51f2573e65ac70d06cd80eef8d4
Fantomita <prisonvlad@gmail.com>
2024-10-23 17:38:42 +0300

    idfk

--- C#/Aplicatie1/Aplicatie1/Form1.Designer.cs
+++ C#/Aplicatie1/Aplicatie1/Form1.Designer.cs
@@ -37,10 +37,18 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -81,7 +89,7 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.LightBlue;
-            this.pictureBox1.Location = new System.Drawing.Point(250, 81);
+            this.pictureBox1.Location = new System.Drawing.Point(70, 81);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(119, 115);
             this.pictureBox1.TabIndex = 3;
@@ -91,7 +99,7 @@
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.LightBlue;
-            this.pictureBox2.Location = new System.Drawing.Point(428, 81);
+            this.pictureBox2.Location = new System.Drawing.Point(263, 81);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(119, 115);
             this.pictureBox2.TabIndex = 4;
@@ -101,7 +109,7 @@
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.LightBlue;
-            this.pictureBox3.Location = new System.Drawing.Point(250, 241);
+            this.pictureBox3.Location = new System.Drawing.Point(70, 241);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(119, 115);
             this.pictureBox3.TabIndex = 5;
@@ -111,12 +119,52 @@
             // pictureBox4
             // 
             this.pictureBox4.BackColor = System.Drawing.Color.LightBlue;
-            this.pictureBox4.Location = new System.Drawing.Point(428, 241);
+            this.pictureBox4.Location = new System.Drawing.Point(263, 241);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(119, 115);
             this.pictureBox4.TabIndex = 6;
             this.pictureBox4.TabStop = false;
             this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.LightBlue;
+            this.pictureBox5.Location = new System.Drawing.Point(452, 81);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(119, 115);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.LightBlue;
+            this.pictureBox6.Location = new System.Drawing.Point(452, 241);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(119, 115);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.LightBlue;
+            this.pictureBox7.Location = new System.Drawing.Point(636, 81);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(119, 115);
+            this.pictureBox7.TabIndex = 9;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.LightBlue;
+            this.pictureBox8.Location = new System.Drawing.Point(636, 241);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(119, 115);
+            this.pictureBox8.TabIndex = 10;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
             // 
             // Form1
             // 
@@ -124,6 +172,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
@@ -138,6 +190,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,6 +209,10 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
     }
 }
 
--- C#/Aplicatie1/Aplicatie1/Form1.cs
+++ C#/Aplicatie1/Aplicatie1/Form1.cs
@@ -15,19 +15,19 @@ namespace Aplicatie1
     {
         int m = 0, s = 0, nr = 0, ok, x = 0;
         Random rnd = new Random();
-        int[] v = new int[5];
-        Image[] img = new Image[5];
+        int[] v = new int[9];
+        Image[] img = new Image[9];
         PictureBox firstClicked = null;
         PictureBox secondClicked = null;
         public Form1()
         {
             InitializeComponent();
             timer1.Start();
-            for (int i = 1; i <= 4; ++i)
+            for (int i = 1; i <= 8; ++i)
             {
                 do
                 {
-                    v[i] = rnd.Next(4) + 1;
+                    v[i] = rnd.Next(8) + 1;
                     ok = 1;
                     for (int j = 1; j < i; ++j)
                         if (v[i] == v[j])
@@ -36,16 +36,24 @@ namespace Aplicatie1
             }
 
             img[1] = Image.FromFile("mar.png");
-            img[4] = Image.FromFile("mar.png");
+            img[8] = Image.FromFile("mar.png");
             img[2] = Image.FromFile("para.png");
-            img[3] = Image.FromFile("para.png");
+            img[7] = Image.FromFile("para.png");
+            img[3] = Image.FromFile("struguri.png");
+            img[6] = Image.FromFile("struguri.png");
+            img[4] = Image.FromFile("banana.png");
+            img[5] = Image.FromFile("banana.png");
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -73,8 +81,30 @@ namespace Aplicatie1
             HandlePictureBoxClick(pictureBox4, 4);
         }
 
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            HandlePictureBoxClick(pictureBox5, 5);
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            HandlePictureBoxClick(pictureBox6, 6);
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            HandlePictureBoxClick(pictureBox7, 7);
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            HandlePictureBoxClick(pictureBox8, 8);
+        }
+
         private async void HandlePictureBoxClick(PictureBox clickedPictureBox, int index)
         {
+            DisableAllPictureBoxes();
+
             nr++;
             //label3.Text = "Clicked on: " + index + " and the number of clicks is: " + nr;
             if (nr % 2 == 1)
@@ -91,7 +121,7 @@ namespace Aplicatie1
                 secondClicked = clickedPictureBox;
 
                 await Task.Delay(500);
-                if (v[x] == 5 - v[index])
+                if (v[x] == 9 - v[index])
                 {
                     firstClicked.Visible = false;
                     secondClicked.Visible = false;
@@ -107,7 +137,33 @@ namespace Aplicatie1
                 firstClicked = null;
                 secondClicked = null;
                 nr = 0;
-            }   
+            }
+
+            EnableAllPictureBoxes();
+        }
+
+        private void DisableAllPictureBoxes()
+        {
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = false;
+            pictureBox3.Enabled = false;
+            pictureBox4.Enabled = false;
+            pictureBox5.Enabled = false;
+            pictureBox6.Enabled = false;
+            pictureBox7.Enabled = false;
+            pictureBox8.Enabled = false;
+        }
+
+        private void EnableAllPictureBoxes()
+        {
+            pictureBox1.Enabled = true;
+            pictureBox2.Enabled = true;
+            pictureBox3.Enabled = true;
+            pictureBox4.Enabled = true;
+            pictureBox5.Enabled = true;
+            pictureBox6.Enabled = true;
+            pictureBox7.Enabled = true;
+            pictureBox8.Enabled = true;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
