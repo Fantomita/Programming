881234220dc2dcb8114817348b653c394330cf39
Fantomita <prisonvlad@gmail.com>
2024-10-25 00:18:15 +0300

    idk

---
+++ C#/freeCodeCamp/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        /*
+            * Define a variable to hold your name
+            * Define a variable to hould your phone number
+            * Define a variable to hold your age
+            * Print variables line-by-line to the screen 
+            * Extra: define variables using the var keyword
+        */
+        static void Main(string[] args)
+        {
+            string name = "Vlad",
+                   phoneNumber = "0746615423";
+            int age = 18;
+
+            Console.WriteLine(name);
+            Console.WriteLine(phoneNumber);
+            Console.WriteLine(age);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/freeCodeCamp/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    /*
+     * Create & initialise two ints
+     * Make a variable to work out the remainder
+     * Output remainder to the screen
+     * Change the first int variable to another number
+        * and recalculate the remainder
+        * output new remainder to the screen
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 123,
+                secondNumber = 24,
+                remainder = firstNumber % secondNumber;
+            Console.WriteLine(remainder);
+
+            firstNumber = 4534;
+            remainder = firstNumber % secondNumber;
+            Console.WriteLine(remainder);
+
+            Console.ReadLine();
+        }
+    }
+}
