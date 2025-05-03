01bccfc2c15be233a7003227e3afc404d505f6ba
Fantomita <56206078+Fantomita@users.noreply.github.com>
2025-05-04 02:12:52 +0300

    Update Sub 13.sql

--- Atestat SQL/Sub 13.sql
+++ Atestat SQL/Sub 13.sql
@@ -2,7 +2,7 @@
 select * from bibliot
 --b
 if exists(select * from bibliot where titlu='Pe culmile disperarii')
-	select autor, editura from bibliot where titlu='Pe culmile diserarii'
+	select autor, editura from bibliot where titlu='Pe culmile disperarii'
 else
 	select 'Cartea cu titlu cerut nu exista in biblioteca' as Mesaj
 --c
